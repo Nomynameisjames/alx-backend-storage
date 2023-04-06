90a84b8b6111fda7aff92ad3f22fd55ce6f3c433
Nomynameisjames <adavaonimisi@gmail.com>
2023-04-06 02:45:31 +0000

    ranking

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -5,15 +5,15 @@
 /* Delimiter is created to prevent the default use of the semi colon which 
   causes problem in stored procedure because a procedure can have many statements
 */
-DROP PROCEDURE IF EXISTS fan_ranking;
-DELIMITER $$
-CREATE PROCEDURE fan_ranking()
-    	BEGIN
+--DROP PROCEDURE IF EXISTS fan_ranking;
+--DELIMITER $$
+--CREATE PROCEDURE fan_ranking()
+--  	BEGIN
 		SELECT origin, SUM(fans) AS nb_fans
     		FROM metal_bands
     		GROUP BY origin
     		ORDER BY nb_fans DESC;
-    	END$$
-CALL fan_ranking()$$
-DELIMITER ;$$
+--    	END$$
+--CALL fan_ranking()$$
+--DELIMITER ;$$
 
