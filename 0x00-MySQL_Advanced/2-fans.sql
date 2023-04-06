ec1f9c925058a7ef9e413906db5c2f25f2eba81c
Nomynameisjames <adavaonimisi@gmail.com>
2023-04-06 02:30:52 +0000

    ranking

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,19 @@
+
+--USE  hbtn_0d_tvshows;
+/* file contains sql script that  that ranks country origins of bands,
+   ordered by the number of (non-unique) fans */
+
+/* Delimiter is created to prevent the default use of the semi colon which 
+  causes problem in stored procedure because a procedure can have many statements
+*/
+DELIMITER $$
+CREATE PROCEDURE fan_ranking() --declaration of procedure with no parameter to rank fans
+	BEGIN
+		SELECT origin, SUM(fans) AS nb_fans
+    		FROM metal_bands
+    		GROUP BY origin
+    		ORDER BY nb_fans DESC;
+	END$$
+CALL fan_ranking()$$  --procedure is called
+DELIMITER ; --delimiter is reverted back to the default semi-colon
+
