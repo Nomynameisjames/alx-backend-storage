671a903ded657a6ddc75e983bbb42705ad9c6710
Nomynameisjames <adavaonimisi@gmail.com>
2023-04-06 01:28:26 +0000

    bug fix that causes crash

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,17 @@
+--USE hbtn_0d_tvshows
+/* create a table users checks if it already exists and creates it if doesn't */
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    	email VARCHAR(255) NOT NULL UNIQUE,
+    	name VARCHAR(255),
+    	country ENUM('CO', 'US', 'TN') NOT NULL DEFAULT 'US'
+);
+
+/* One of the distinct datatype used in this file
+   is the ENUM in the country column which enables it
+   to have a set likely value of occurance,
+   sets the default country to US and ensure one of
+   either values set is used in the  column 
+
+*/
