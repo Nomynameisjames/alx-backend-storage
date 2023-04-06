a51d0dc140b36a27071d92573f5459209f2e110a
Nomynameisjames <adavaonimisi@gmail.com>
2023-04-06 00:18:31 +0000

    creating tables with constraints

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,3 @@
--- USE hbtn_0d_tvshows;
-/* selects MYSQL database to utilize */
-
--- DROP TABLE IF EXISTS users;
-/* checks the newly selected database if a users table exists if it does it deletes */
-
 CREATE TABLE users (
 	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
