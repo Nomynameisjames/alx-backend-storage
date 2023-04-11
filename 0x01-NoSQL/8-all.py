b356cad7cb01cafe75a05f98f2af0bd59544ff94
Nomynameisjames <adavaonimisi@gmail.com>
2023-04-11 16:19:07 -0700

    using the pymongo module to list all items in a collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,9 +2,13 @@
 '''
     using the pymongo module to list all items in a mongoDB collection
 '''
+from typing import List
 
 
-def list_all(mongo_collection):
+def list_all(mongo_collection) -> List:
+    '''
+        query the collection using the find method
+    '''
     db = mongo_collection.find()
     collection = [items for items in db]
     return collection
