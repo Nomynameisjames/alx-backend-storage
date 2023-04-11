9331b79c35e79b198621144ccacf1ed603f00039
Nomynameisjames <adavaonimisi@gmail.com>
2023-04-11 15:17:36 -0700

    using the pymongo module to list items in a collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,9 +3,6 @@
     using the pymongo module to list all items in a mongoDB collection'''
 from pymongo import MongoClient
 from typing import List
-'''def connect():
-    client = MongoClient(host="localhost", port=27017)
-    return client'''
 
 
 def list_all(mongo_collection) -> List:
