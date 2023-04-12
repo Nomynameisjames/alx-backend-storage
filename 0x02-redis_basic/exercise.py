913db4296e0b11818ae3f62825f59fed62648d68
Nomynameisjames <adavaonimisi@gmail.com>
2023-04-12 23:48:17 +0000

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,18 +7,21 @@ from typing import Union
     import uuid to generate random numbers
     import Union from typing to annotate various datatypes
 '''
+
+
 difData = Union[int, float, str, bytes]
+
+
 class Cache:
-'''
-    class creates an instance of the Redis saves in a private class
-    attribute and generates a random key using the uuid4 method
-'''
-   def __init__(self):
-       self._redis = redis.Redis()
-       self._redis.flushdb()
+    '''
+        class creates an instance of the Redis saves in a private class
+        attribute and generates a random key using the uuid4 method
+    '''
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data: difData) -> str:
-        key = str(uuid.uuid4())
+        key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-
